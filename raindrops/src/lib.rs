0859d2a6ebc02094df9118057d381597d3265d4e
Ryan Matlock <ryan.matlock@gmail.com>
2023-06-14 13:54:45 -0700

    :construction: attempt using iter, filter, and map

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,7 +1,18 @@
-pub fn raindrops(n: u32) -> () {
+pub fn raindrops(n: u32) -> String {
     let sounds: [(u32, &str); 3] = [
         (3, "Pling"),
         (5, "Plang"),
         (7, "Plong")
     ];
+    let sounds = sounds.iter()
+        .filter(|(divisor, _)| *n % *divisor == 0)
+        .map(|(_, sound)| *sound.to_string());
+    // match sounds.join("") {
+    //     "" => n.to_string(),
+    //     other => sounds.join("").to_string(),
+    // }
+    match sounds {
+        Some(arr) => arr,
+        None => [String; 0],
+    }
 }
