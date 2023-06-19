d02cf06244b1e40077661b665e05c8e6995dd192
Ryan Matlock <ryan.matlock@gmail.com>
2023-06-19 16:51:58 -0700

    :white_check_mark: pass all tests

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,19 @@
+const BOARD_MIN: u32 = 1;
+const BOARD_MAX: u32 = 64;
+
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {s}");
+    const BASE: u64 = 2;
+
+    if BOARD_MIN <= s && s <= BOARD_MAX {
+        BASE.pow((s - 1).into())
+    } else {
+        panic!("Square must be between 1 and 64")
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    let board: Vec<u32> = (BOARD_MIN..=BOARD_MAX).collect();
+    board.iter()
+        .map(|n| square(*n))
+        .sum()
 }
