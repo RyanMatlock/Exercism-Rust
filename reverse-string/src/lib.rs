5d19e32d255b77f27ed84f7ac2a27d817479397d
Ryan Matlock <ryan.matlock@gmail.com>
2023-06-22 17:12:54 -0700

    :recycle: immutably concatenate vectors

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -2,13 +2,15 @@ fn reverse_helper<'a>(s: &'a str, acc: Vec<&'a str>) -> Vec<&'a str> {
     if s.is_empty() {
         return acc
     } else {
-        // let mut chars = s.chars();
-        // let mut buffer = [0; 4];
-        // let c = chars.next().expect("slice s isn't empty").encode_utf8(&mut buffer);
-        // let rest = chars.collect::<Vec<_>>();
-        let mut new_vec = vec!(&s[0..1]);
-        new_vec.extend(acc);
-        reverse_helper(&s[1..], new_vec)
+        let mut chars = s.chars();
+        let c = chars
+            .next()
+            .expect("slice s isn't empty")
+            .to_string()
+            .as_str();
+        let rest = chars.as_str();
+        let new_acc = [&vec!(c)[..], &acc].concat();
+        reverse_helper(rest, new_acc)
     }
 }
 
